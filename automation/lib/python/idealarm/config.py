d42499047292b68ed3f754e7fa093ac020d0a57c
ricadelic Delix <ricadelic@gmail.com>
2018-06-03 13:39:20 +0200

    alarm items added

--- automation/lib/python/idealarm/config.py
+++ automation/lib/python/idealarm/config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from org.eclipse.smarthome.core.library.types import OnOffType
 from org.eclipse.smarthome.core.types import UnDefType
+
 ON = OnOffType.ON
 OFF = OnOffType.OFF
 NULL = UnDefType.NULL
@@ -27,11 +28,16 @@ ALARM_ZONES = [
         'statusItem': 'Z1_Status',
         'alertDevices': ['toggle_notification_message_pushover_alarm'],
         'sensors': [
-            {'name': 'door_keuken_sensor',                          'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
-            {'name': 'sensor_voordeur_switch_status',               'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
-            {'name': 'sensor_achterdeur_switch_status',             'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
-            {'name': 'sensor_spacecave_raam_links_switch_status',   'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
-            {'name': 'sensor_spacecave_raam_rechts_switch_status',  'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True}
+            {'name': 'door_keuken_sensor',                              'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_voordeur_switch_status',                   'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_achterdeur_switch_status',                 'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_schuifpui_switch_status',                  'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_kantoor_raam_switch_status',               'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_slaapkamer_raam_switch_status',            'sensorClass': 'B', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_sunny_switch_status',                      'sensorClass': 'B', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_opslag_raam_switch_status',                'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_spacecave_raam_links_switch_status',       'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True},
+            {'name': 'sensor_spacecave_raam_rechts_switch_status',      'sensorClass': 'A', 'nag': False,   'nagTimeoutMins': 4, 'armWarn': True,  'enabled': True}
         ],
         'armAwayToggleSwitch': 'Toggle_Z1_Armed_Away',
         'armHomeToggleSwitch': 'Toggle_Z1_Armed_Home',
